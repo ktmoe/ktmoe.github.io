638f6ec0ba017f33c220385044faa7dd6fa416ec
Kay T. Moe <55950227+ktmoe@users.noreply.github.com>
2021-12-15 12:35:53 +0630

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "78e9e73c52839b016f7bd759dabe26b6",
-"assets/assets/icons/Social.ttf": "e72652d0e803d14f835a84358ed3333f",
-"assets/assets/images/profile.jpg": "b19aa8de38f316ba81536da0d3bc88bb",
-"assets/assets/images/spaceman.jpg": "8947106cca21d34ee583ee1a6328b050",
-"assets/FontManifest.json": "06f774ff0d6a5ae9b9a662fb259ab679",
+  "assets/AssetManifest.json": "e9506894e636e27227d4e47323f12b58",
+"assets/FontManifest.json": "3c9d60aa1ab214b76d78987044f6d9c4",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/icons/Social.ttf": "e72652d0e803d14f835a84358ed3333f",
+"assets/images/profile.jpg": "b19aa8de38f316ba81536da0d3bc88bb",
+"assets/images/spaceman.jpg": "8947106cca21d34ee583ee1a6328b050",
 "assets/NOTICES": "72990abc9589195e737ccd9eab4ee71c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "67885ccb5c373c366f66b538a7665c1f",
-"/": "67885ccb5c373c366f66b538a7665c1f",
-"main.dart.js": "be459e78a233af98e45b40b41d40252f",
+"index.html": "df346381f47a670e4f2aee4e2d0f40e1",
+"/": "df346381f47a670e4f2aee4e2d0f40e1",
+"main.dart.js": "4ab0f79804ced04c80daa0adca400a40",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
